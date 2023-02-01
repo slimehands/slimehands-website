c092fde98502e07130235fb432941871c7a51798
vmdoi <rodriguesorlen@gmail.com>
2023-02-01 21:46:14 +0000

    maybe this will fix it?

--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,9 @@ const config = {
 
   kit: {
     adapter: adapter({"strict":false}),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+      },
   },
 };
 
